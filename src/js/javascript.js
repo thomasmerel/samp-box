000f4e5490a82f1c95ebf7b51fec54984c006b9f
thomas <thomas.merel.44@gmail.com>
2017-03-01 15:50:29 +0100

    fix json

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,25 +1,26 @@
 $(function(){
         
     var jsonURL = 'http://sampbox.thomas-merel.fr/src/config/buttons.json'
-        
-    getJSON(jsonURL);
-        
+    console.log(jsonURL);
+	getJSON(jsonURL);
 });
 
 function getJSON(url){
     $.getJSON( url, function(json) {
+	console.log(json);
         managerJSON(json);
     });
 }
 
 function managerJSON(jsonFile){
     Object.keys(jsonFile).forEach(function(key){
+	console.log(key);
         htmlConstructor({name: key, value: jsonFile[key]});
     });
 }
 
 function htmlConstructor(jsonButton){
-
+    console.log(jsonButton);
     var button = '<a class="waves-effect waves-light btn" onClick="play(this)" data-button-name="'+jsonButton.name+'" data-sound-uri="'+jsonButton.value+'" href="#">'+jsonButton.name+'</a>';
     $('#buttons-lib').append(button);
 }
