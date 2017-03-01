e6eee18538685416d2d29a72e5d7d331ed15f8fc
Thomas MEREL <thomas.merel.44@gmail.com>
2017-03-01 15:32:24 +0100

    update all

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,16 +1,25 @@
 $(function(){
-    var jsonPath = "http://sampbox.thomas-merel.fr/src/config/buttons.json?jsoncallback=?";
-    readJson(jsonPath);
+        
+    var jsonURL = 'http://soundboard.thomas-merel.fr/config.json'
+        
+    getJSON(jsonURL, initialStep);
+        
 });
 
-
-function readJson(path){
-    $.getJSON(path, function(json) {
-        readData(jsonObject);
+function getJSON(url){
+    $.getJSON( url, function(json) {
+        managerJSON(json);
     });
 }
 
-function readData(data){
-    jsonObject = JSON.parse(data);
-    console.log(jsonObject);
+function managerJSON(jsonFile){
+    Object.keys(jsonFile).forEach(function(key){
+        htmlConstructor({name: key, value: jsonFile[key]});
+    });
+}
+
+function htmlConstructor(jsonButton){
+
+    var button = '<a class="waves-effect waves-light btn" onClick="play(this)" data-button-name="'+jsonButton.name+'" data-sound-uri="'+jsonButton.value+'" href="#">'+jsonButton.name+'</a>';
+    $('#button-lib').append(button);
 }
