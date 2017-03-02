899593b931a8abd7ff71e1d18560b8affbc21b9a
Thomas MEREL <thomas.merel.44@gmail.com>
2017-03-02 14:38:23 +0100

    fix json

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -13,22 +13,20 @@ function getJSON(url){
 }
 
 function managerJSON(jsonFile){
-    Object.keys(jsonFile).forEach(function(key){
-	console.log(key);
-        htmlConstructor({name: key, value: jsonFile[key]});
+    jsonFile.forEach(function(element){
+        htmlConstructor(element);
     });
 }
 
 function htmlConstructor(jsonButton){
     console.log(jsonButton);
-    var button = '<a class="waves-effect waves-light btn" onClick="play(this)" data-button-name="'+jsonButton.name+'" data-sound-uri="'+jsonButton.value+'" href="#">'+jsonButton.name+'</a>';
+    var button = '<a class="waves-effect waves-light btn" onClick="play(this)" data-button-tags="'+jsonButton.tag+'" data-button-name="'+jsonButton.text+'" data-sound-uri="'+jsonButton.uri+'" href="#">'+jsonButton.text+'</a>';
     $('#buttons-lib').append(button);
 }
 
 function play(button){
     console.log(button);
-	var url = button.getAttribute('data-sound-uri');
-	new Audio(url).play();
+    new Audio('<url>').play();
 }
 
 
